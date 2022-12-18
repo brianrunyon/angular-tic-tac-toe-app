33a4a3f21eec982f8d285e02fc2df6ecb245818b
brianrunyon <brian.runyon+1@gmail.com>
2022-12-18 07:37:39 -0500

    Update checkBoard validation logic

--- src/common/Board.ts
+++ src/common/Board.ts
@@ -9,6 +9,57 @@ interface GameState {
   winnerPlayer: ('' | 'X' | 'O');
 }
 
+interface ValidationState {
+  consecutiveSpots: number;
+  checkingPlayer: ('' | 'X' | 'O');
+  reset: () => void;
+  checkSpot: (spot: ('' | 'X' | 'O')) => boolean;
+  checkWinner: (state: GameState) => boolean;
+}
+
+const getInitialValidationState = () => {
+  let consecutiveSpots: number = 0;
+  let checkingPlayer: ('' | 'X' | 'O') = '';
+  const reset = () => {
+    consecutiveSpots = 0;
+    checkingPlayer = '';
+  };
+  const checkSpot = (spot: ('' | 'X' | 'O')) => {
+    if(spot !== '') {
+      if(checkingPlayer === '') {
+        checkingPlayer = spot;
+        consecutiveSpots++;
+        return true;
+      }
+      else if(checkingPlayer === spot) {
+        consecutiveSpots++;
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  };
+  const checkWinner = (state: GameState) => {
+    if(consecutiveSpots === 3) {
+      state.winnerPlayer = checkingPlayer;
+      return true;
+    }
+    return false;
+  };
+  const validationState: ValidationState = {
+    consecutiveSpots,
+    checkingPlayer,
+    reset,
+    checkSpot,
+    checkWinner
+  };
+  return validationState;
+}
+
 const getInitialGameState = () => {
   let state: GameState = {
     currentPlayer: 'X',
@@ -25,10 +76,82 @@ const getInitialGameState = () => {
 const checkBoard = (state: GameState) => {
   let board: ('' | 'X' | 'O')[][] = state.currentBoard;
 
-  // check each row
-  // check each column
-  // check diagonals
+  let validationState = getInitialValidationState();
 
+  // check each row
+  for(let r=0; r<ROWS; r++) {
+    const row: ('' | 'X' | 'O')[] = board[r];
+
+    validationState.reset();
+
+    for(let c=0; c<COLUMNS; c++) {
+      const spot: ('' | 'X' | 'O') = row[c];
+      if(!validationState.checkSpot(spot)) {
+        break;
+      }
+    }
+
+    if(validationState.checkWinner(state)) {
+      console.log(`(row) winner ${validationState.checkingPlayer}`);
+      return state;
+    }
+
+  }
+
+  // check each column
+  for(let c=0; c<COLUMNS; c++) {
+
+    validationState.reset();
+
+    for(let r=0; r<ROWS; r++) {
+      const row: ('' | 'X' | 'O')[] = board[r];
+      const spot: ('' | 'X' | 'O') = row[c];
+      if(!validationState.checkSpot(spot)) {
+        break;
+      }
+    }
+
+    if(validationState.checkWinner(state)) {
+      console.log(`(column) winner ${validationState.checkingPlayer}`);
+      return state;
+    }
+
+  }
+
+  // check diagonals
+  {
+    validationState.reset();
+
+    for(let r=0, c=0; r<ROWS && c<COLUMNS; r++, c++) {
+      const spot: ('' | 'X' | 'O') = board[r][c];
+      if(!validationState.checkSpot(spot)) {
+        break;
+      }
+    }
+
+    if(validationState.checkWinner(state)) {
+      console.log(`(diagonal top left) winner ${validationState.checkingPlayer}`);
+      return state;
+    }
+
+  }
+
+  {
+    validationState.reset();
+
+    for(let r=0, c=COLUMNS-1; r<ROWS && c>=0; r++, c--) {
+      const spot: ('' | 'X' | 'O') = board[r][c];
+      if(!validationState.checkSpot(spot)) {
+        break;
+      }
+    }
+
+    if(validationState.checkWinner(state)) {
+      console.log(`(diagonal top right) winner ${validationState.checkingPlayer}`);
+      return state;
+    }
+
+  }
   return state;
 };
 
