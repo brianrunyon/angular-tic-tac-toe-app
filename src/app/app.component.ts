138c4a76ef699c185ce796f51e50858f548d25f6
brianrunyon <brian.runyon+1@gmail.com>
2022-12-18 07:37:39 -0500

    add onSpotClick logic to update game state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,28 @@ export class AppComponent {
   state: GameState = getInitialGameState();
 
   onSpotClick(row: number, column: number) {
-    
+    const currentBoard: ('' | 'X' | 'O')[][] = this.state.currentBoard;
+    const winnerPlayer: ('' | 'X' | 'O') = this.state.winnerPlayer;
+    const currentPlayer: ('X' | 'O') = this.state.currentPlayer;
+    const nextPlayer: ('X' | 'O') = (currentPlayer === 'X') ? 'O' : 'X';
+
+    // check if the game already has a winner
+    if(winnerPlayer !== '') {
+      console.log(`game already has a winner (winner: ${winnerPlayer}`);
+      return;
+    }
+
+    // check if the spot clicked is blank
+    let previousValue = currentBoard[row][column];
+    if(previousValue === '') {
+      let newValue = currentPlayer;
+      currentBoard[row][column] = newValue;
+
+      this.state.currentPlayer = nextPlayer;
+
+      // update the game state and check if there is a winner
+      this.state = checkBoard(this.state);
+    }
   }
 }
 
